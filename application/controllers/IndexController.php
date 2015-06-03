dbfc5890e59bf620bd22c1ed80a763e7981c1948
yeawin <yeawin@xmu.edu.cn>
2015-06-03 21:06:18 +0800

    添加完善成功案例

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -12,13 +12,14 @@ class IndexController extends Zend_Controller_Action
     {
         // action body
         $this->view->headScript()
-        ->prependFile($this->view->baseUrl("/static/jquery.plugins/ie10-viewport-bug-workaround.js"))
-        ->prependFile($this->view->baseUrl("/static/jquery.plugins/bootstrap/js/bootstrap.min.js"))
-        ->prependFile($this->view->baseUrl("/static/jquery.plugins/jquery.migrate.js"))
-        ->prependFile($this->view->baseUrl("/static/jquery.plugins/jquery.js"));
+            ->prependFile($this->view->baseUrl("/static/js/main.js"))
+            ->prependFile($this->view->baseUrl("/static/jquery.plugins/ie10-viewport-bug-workaround.js"))
+            ->prependFile($this->view->baseUrl("/static/jquery.plugins/bootstrap/js/bootstrap.min.js"))
+            ->prependFile($this->view->baseUrl("/static/jquery.plugins/jquery.migrate.js"))
+            ->prependFile($this->view->baseUrl("/static/jquery.plugins/jquery.js"));
         $this->view->headLink()
-        ->appendStylesheet($this->view->baseUrl("/static/jquery.plugins/bootstrap/css/bootstrap.min.css"))
-        ->appendStylesheet($this->view->baseUrl("/static/css/style.css"));
+            ->appendStylesheet($this->view->baseUrl("/static/jquery.plugins/bootstrap/css/bootstrap.min.css"))
+            ->appendStylesheet($this->view->baseUrl("/static/css/style.css"));
     }
 
     
---
+++ public/static/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+$(function(){
+	//限制字符个数
+//    $(".cgal-text-overflow").each(function(){
+//        var maxwidth = 64;
+//        var well = $(this).parent().find(".well");
+//        if(well.text().length > maxwidth){
+//            var text = well.text().substring(0, maxwidth);
+//            $(this).html(text + '...');
+//        }
+//    });
+});
